20bae39f0033433bcfea93b8bb755fd1ff348cb4
Rhys-Baker <bakerrhys05@gmail.com>
2025-12-11 21:51:00 +0800

    Make setcolor public

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stddef.h>
 
+extern bool escaped;
 void terminal_init(void);
+void terminal_setcolor(uint8_t color);
 void terminal_putchar(char c);
 void terminal_write(const char *data, size_t size);
 void terminal_writestring(const char *data);
