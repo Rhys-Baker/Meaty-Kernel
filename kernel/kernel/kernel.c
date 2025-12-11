0b134f435d3d3003c0970d35652dd76f8b29b220
Rhys-Baker <bakerrhys05@gmail.com>
2025-12-11 21:53:17 +0800

    update main function to test new printf

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -8,6 +8,8 @@ const char *testString = "This is a test string.\nIt tests \x1B\x0AMOST\x1B\x0F 
 void kernel_main(void){
     terminal_init();
     printf("Hello, World!\n");
-    printf("%c", testString);
+    printf("Test char: %c %c %c\n", 'A', 'B', 'C');
+    printf("Test string: %s\n", testString);
+    printf("Hex number: 0x%x\n", 0x1234ABCD);
     printf("\nDone.\n");
 }
