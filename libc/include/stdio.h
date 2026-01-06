9ecb20d8d43a9339256e304cf6c5abe68bc2a950
Rhys-Baker <bakerrhys05@gmail.com>
2026-01-06 14:27:25 +0800

    Update printf to support decimal numbers

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,12 +1,25 @@
 #pragma once
 
 #include <sys/cdefs.h>
+#include <stddef.h>
 
 #define EOF (-1)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef int (*pfnStreamWriteBuf)(char*);
+
+typedef struct Stream{
+	size_t buf_len;
+	size_t buf_i;
+	char *buf;
+	pfnStreamWriteBuf pfn_write_all;
+} Stream;
+
+extern Stream stdout;
+
 
 int printf(const char *__restrict, ...);
 int putchar(int);
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -32,12 +32,18 @@ int printf(const char *restrict format, ...){
                     char *str = (char *)va_arg(args, int);
                     print(str, strlen(str));
                     break;
+				case 'd':
+					char intstr[12]; // 1 for sign, 11 for value, 1 for null
+					unsigned int d_integer = va_arg(args, int);
+					itoa(intstr, d_integer, 10);
+					print(intstr, strlen(intstr));
+					break;
                 case 'x':
                     char hexstr[9];    
-                    unsigned int integer = va_arg(args, int);
+                    unsigned int x_integer = va_arg(args, int);
                     for(int i=0; i<8; i++){
-                        unsigned int nibble = (integer >> (28 - 4*i)) & 0xF;
-                        hexstr[i] = (nibble < 10) ? ('0' + nibble) : ('A' + nibble-10);
+                        unsigned int x_nibble = (x_integer >> (28 - 4*i)) & 0xF;
+                        hexstr[i] = (x_nibble < 10) ? ('0' + x_nibble) : ('A' + x_nibble-10);
                     }
                     hexstr[8] = '\0';
                     print(hexstr, 8);
